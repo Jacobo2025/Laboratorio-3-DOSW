24303d6412abc3c77211cc325624c68203ad9449
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-08-31 18:13:24 -0500

    Parte 1 - Jacobo Diaz Alvarado: se añadieron dependencias y pluglins

--- src/main/java/edu/dosw/lab/Application.java
+++ src/main/java/edu/dosw/lab/Application.java
@@ -1,13 +1,13 @@
-package edu.dosw.lab.Laboratorio_3_DOSW;
+package edu.dosw.lab;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Laboratorio3DoswApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Laboratorio3DoswApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
