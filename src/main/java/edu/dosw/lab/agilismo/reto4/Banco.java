c6d73857d5ab806a6af413921ee2dc6d55d6e9d2
Jacoboo <jacobo.diaz-a@mail.escuelaing.edu.co>
2025-09-01 17:26:17 -0500

    Punto2 - Jacobo Diaz: refactorización de las clase del reto 4

--- src/main/java/edu/dosw/lab/agilismo/reto4/Banco.java
+++ src/main/java/edu/dosw/lab/agilismo/reto4/Banco.java
@@ -1,4 +1,19 @@
-package edu.dosw.lab.agilismo.reto4;
+package bankify;
 
 public class Banco {
+    private String codigoBanco;
+    private String nombreBanco;
+
+    public Banco(String codigoBanco, String nombreBanco) {
+        this.codigoBanco = codigoBanco;
+        this.nombreBanco = nombreBanco;
+    }
+
+    public String getCodigoBanco() {
+        return codigoBanco;
+    }
+
+    public String getNombreBanco() {
+        return nombreBanco;
+    }
 }
--- src/main/java/edu/dosw/lab/agilismo/reto4/GestorCuenta.java
+++ src/main/java/edu/dosw/lab/agilismo/reto4/GestorCuenta.java
@@ -1,5 +1,22 @@
-package edu.dosw.lab.agilismo.reto4;
+package bankify;
+
 
 public class GestorCuenta {
 
+    public CuentaBancaria crearCuenta(Cliente cliente, Banco banco, String numeroCuenta) {
+        return new CuentaBancaria(numeroCuenta, banco, cliente);
+    }
+
+    public boolean validarCuenta(CuentaBancaria cuenta) {
+        return cuenta.validarCuenta();
+    }
+
+    public int consultarSaldo(CuentaBancaria cuenta) {
+        return cuenta.getSaldo();
+    }
+
+    public void realizarDeposito(CuentaBancaria cuenta, int monto) {
+        cuenta.depositar(monto);
+    }
 }
+
